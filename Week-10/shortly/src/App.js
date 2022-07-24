3f02ed294f8a8e7775946a6f6b3971ed90c679d9
Yogesh Rai <yograi343@gmail.com>
2022-07-23 23:41:51 -0700

    Shorli website is completed

--- Week-10/shortly/src/App.js
+++ Week-10/shortly/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Body from "./components/Body";
+import Display from "./components/Display";
+import { useState } from "react";
+import Footer from "./components/Footer";
 
 function App() {
+  const [link, setLink] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Body setLink={setLink} />
+      <Display link={link} />
+      <Footer />
+    </>
   );
 }
 
