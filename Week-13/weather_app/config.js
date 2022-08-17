ee11377a777a36992dfea5f1911367a776595eeb
Yogesh Rai <yograi343@gmail.com>
2022-08-17 01:13:56 -0700

    Weather app is completed

---
+++ Week-13/weather_app/config.js
@@ -0,0 +1,7 @@
+const constants={
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "a88c1677e9c115e5261956513263e25a"
+    }
+}
+module.exports = constants;
